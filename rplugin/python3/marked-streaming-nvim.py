941c146f6a77ce126f438e4c7502cbb29bbe85ea
thecontinium <thecontinium@outlook.com>
2021-04-23 16:35:32 +0100

    streaming working

--- rplugin/python3/marked-streaming-nvim.py
+++ rplugin/python3/marked-streaming-nvim.py
@@ -1,11 +1,16 @@
 import neovim
+import sys
 
 @neovim.plugin
 class Main(object):
     def __init__(self, vim):
         self.vim = vim
 
-    @neovim.function('DoItPython')
-    def doItPython(self, args):
-        self.vim.command('echo "hello from DoItPython"')
+    @neovim.function('StreamToMarked')
+    def stream_to_marked(self, arg):
+        from AppKit import NSPasteboard
+        buffer = "\n".join(self.vim.call('getline','1','$'))
+        pb = NSPasteboard.pasteboardWithName_("mkStreamingPreview")
+        pb.clearContents()
+        pb.setString_forType_(buffer, 'public.utf8-plain-text')
 
