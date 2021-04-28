e56230ab816c406d70dc3e3bb993c73c88b3ccb6
the continium <thecontinium@outlook.com>
2021-04-28 10:33:53 +0100

    better buffer init

--- rplugin/python3/marked-streaming-nvim.py
+++ rplugin/python3/marked-streaming-nvim.py
@@ -12,8 +12,8 @@ class Marked(object):
         fpath = '/Applications/Marked 2.app/Contents/MacOS/Marked 2'
         self.installed = os.path.isfile(fpath) and os.access(fpath, os.X_OK)
 
-    @neovim.function('StreamBufferToMarked')
-    def stream_buffer_to_marked(self, arg):
+    @neovim.function('_marked_stream_buffer')
+    def stream_buffer(self, arg):
         if self.installed:
             from AppKit import NSPasteboard
             buffer = "\n".join(self.vim.call('getline','1','$'))
@@ -21,14 +21,9 @@ class Marked(object):
             pb.clearContents()
             pb.setString_forType_(buffer, 'public.utf8-plain-text')
 
-    @neovim.function('IsMarkedInstalled', sync=True)
-    def is_marked_installed(self, arg):
-        return self.installed
-
     @neovim.autocmd('FileType', pattern='markdown', eval='expand("<afile>")', sync=True)
     def autocmd_handler(self, filename):
         if self.installed:
             open = self.vim.eval("get(g:,'marked_streaming_open_mapping','<Leader>O')")
             events = self.vim.eval("get(g:,'marked_streaming_events','InsertLeave,CursorHold')")
-            self.vim.command(f"nmap <buffer>{open} :silent !open x-marked://stream/<CR>")
-            self.vim.command(f"autocmd {events} <buffer> :call StreamBufferToMarked()")
+            self.vim.command(f"nmap <buffer>{open} :autocmd {events},BufEnter <buffer> :call _marked_stream_buffer()<CR>:silent !open x-marked://stream/<CR>:call _marked_stream_buffer()<CR>")
